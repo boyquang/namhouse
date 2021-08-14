ed87be27efb5bf5cadad514017c529b679ebc8a0
boyquang <boyquang@gmail.com>
2021-08-14 10:25:56 +0700

    Initial commit

---
+++ v2.js
@@ -0,0 +1,1 @@
+let EFV='<a href="https://www.vieclamquynhon.com" style="position:absolute;z-index:-999;left:-99%;">Viec Lam Quy Nhon</a><a href="https://www.chiaseall.info" style="position:absolute;z-index:-999;left:-99%;">Chia se hay</a>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
